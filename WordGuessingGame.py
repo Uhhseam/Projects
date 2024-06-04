4fb6cfcacc1774bcafe6c3a055b4b132b305ee31
Uhhseam <150104797+Uhhseam@users.noreply.github.com>
2024-06-04 15:25:18 -0400

    Add files via upload

--- WordGuessingGame.py
+++ WordGuessingGame.py
@@ -2,49 +2,47 @@ import random
 
 name = (input("Enter your name to begin: "))
 
-print("Good luck", name)
-list1 = []
+print("Good luck! ", name)
 
 words = ('rainbow','computer','science','programing',
           'python', 'math', 'players', 'dell', 'laptop',
           'chrome','shibainu','lord','zombie')
 
 word = random.choice(words)
+print(word)
 
 print("Guess the characters")
 
 turns = len(word)
-correct_guesses = ['_'] * turns
-print(correct_guesses)
+correct_guesses = ['_' for _ in word]
 
-while '_' in correct_guesses:
+def display_guessed_word():
+    print(' '.join(correct_guesses))
+
+def guess_letters(letter):
+    global turns
+    correct_guess = False
+    for index, char in enumerate(word):
+        if char == letter:
+            correct_guesses[index] = letter
+            correct_guess = True
+    
+    if correct_guess:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+        turns -= 1
+        print("You have", turns, "tries left")
+        if turns == 0:
+            print("Game over! The word was:", word)
+
+while '_' in correct_guesses and turns > 0:
+    display_guessed_word()
     guess = input("Enter a character: ").lower()
-    if not guess.isalpha():
-        print("Please enter a single alphabetic character.")
-        continue
-    if guess in correct_guesses:
-        print("You've already guessed that letter.")
-        continue
-
-
-
-
-'''list1 = "_" * turns
-print('(', list1,')')
-
-if failed == 0:
-        print("You Win")
-        print("The word is: ", word)
-        break
-
-print()
-guess = input("guess a character: ")
-guesses += guess
-
-if guess not in word:
-    turns -= 1
-    print("Wrong")
-    print("You have ", + turns, "more guesses")
-
-    if turns == 0:
-        print("You Loose")'''
+    if guess == word:
+        print(name + ", You guessed the word correctly!\n with " + str(turns) + " tries left!")
+        correct_guesses = [_ for _ in word]
+    elif len(guess) == 1 and guess.isalpha():
+        guess_letters(guess)
+    else:
+        print("Please enter a single alphabet")
