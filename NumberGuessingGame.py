e7e329e2eed85c9d1341acd0ae68f8af799f944b
Uhhseam <150104797+Uhhseam@users.noreply.github.com>
2024-06-12 09:24:24 -0400

    Add files via upload
    Correctly checks whether lower and upper bound are integers, instead of looping over both of them when one was not an integer

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -2,9 +2,9 @@ import math
 import random
 
 def main():
-    lower, upper = check()
+    lower, upper = lowerUpperCheck()
     x = random.randint(lower, upper)
-    print("\n\tYou've only ",
+    print("\n\tYou only have",
             round(math.log(upper - lower + 1,2)),
             "chances to guess the integer! \n")
 
@@ -24,25 +24,44 @@ def main():
             print("You guessed too high!")
 
     if count >= math.log(upper - lower + 1,2):
-        print("\nThe number is %d" % x)
-        print("\tBetter Luck Next Time!")
+        print("The number is %d" % x)
+        print("\n\tBetter Luck Next Time!")
 
-def check():
+def lowerUpperCheck():
+    value1 = value2 = "incorrect"
+    while value1 == "incorrect":
+        try:
+            lower = int(input("Enter Lower bound number: "))
+        except:
+            print("\nInvalid option, Please input an Integer\n")
+        else:
+            value1 = "correct"
+    while value2 == "incorrect":
+        try:
+            upper = int(input("Enter Upper bound number: "))
+        except:
+            print("\nInvalid option, Please input an Integer\n")
+        else:
+            value2 = "correct"
+    return lower, upper
+
+def numberCheck():
     value = "incorrect"
     while value == "incorrect":
         try:
-            lower = int(input("Enter Lower bound: "))
-            upper = int(input("Enter Upper bound: "))
+            guess = int(input("Guess a number: "))
         except:
             print("\nInvalid option, Please input an Integer\n")
         else:
             value = "correct"
-    return lower, upper
-    
+    return guess
 def loop():
+    print("Welcome to the number guessing game!\n")
     again = "y"
     while again == "y":
         main()
-        again = input("Enter 'y' to play again or 'n' to quit: ")
+        again = input("\nEnter 'y' to play again or 'n' to quit: ")
+        if again == 'y':
+            print('\n')
 
 loop()
